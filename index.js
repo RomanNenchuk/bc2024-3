ee04788513e6df081215d872726a3187e2e8d1ce
Roman Nenchuk bc2024-3 <rmnnenchuk@gmail.com>
2024-09-26 00:45:55 +0300

    Created command line program that reads a json file and outputs the results depending on the parameters

--- index.js
+++ index.js
@@ -12,12 +12,32 @@ const input = options.input;
 const output = options.output;
 const display = options.display;
 
-if (!input) console.error("Please, specify input file");
-
-if (!fs.existsSync(input)) console.error("Cannot find input file");
-
-if (!(output || display)) return;
-
-if (output && display) {
-    // ...
+if (!input) {
+    console.error("Please, specify input file");
 }
+
+if (!fs.existsSync(input)) {
+    console.error("Cannot find input file");
+}
+
+if (!(output || display)) {
+    return;
+}
+
+const jsonStr = fs.readFileSync(input, {
+    encoding: "utf-8",
+    flag: "r",
+});
+const data = JSON.parse(jsonStr);
+const newData = data
+    .filter((item) => item.parent === "BS3_BanksLiab")
+    .map((item) => `${item.txten}:${item.value}`);
+const newjsonStr = JSON.stringify(newData);
+
+if (output) {
+    fs.writeFileSync(output, newjsonStr, {
+        encoding: "utf-8",
+    });
+}
+
+if (display) console.log(newjsonStr);
